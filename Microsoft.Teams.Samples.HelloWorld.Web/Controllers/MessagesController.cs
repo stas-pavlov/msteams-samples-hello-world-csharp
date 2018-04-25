fc6953252ea9433552c08f9e4010188c383f4f4d
Stas Pavlov <stanisp@microsoft.com>
2018-04-25 14:03:59 -0700

    add IO.Swagger

--- Microsoft.Teams.Samples.HelloWorld.Web/Controllers/MessagesController.cs
+++ Microsoft.Teams.Samples.HelloWorld.Web/Controllers/MessagesController.cs
@@ -3,21 +3,76 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
 
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Connector.Teams;
 using Microsoft.Bot.Connector.Teams.Models;
+
+using IO.Swagger.Api;
+using IO.Swagger.Client;
+using IO.Swagger.Model;
+
 
 namespace Microsoft.Teams.Samples.HelloWorld.Web.Controllers
 {
     [BotAuthentication]
     public class MessagesController : ApiController
     {
+        public static TriviaApi triviaApi = null;
+
+        
+
         [HttpPost]
         public async Task<HttpResponseMessage> Post([FromBody] Activity activity)
         {
+
+            if (triviaApi == null)
+                triviaApi = new IO.Swagger.Api.TriviaApi();
+
+            bool addedBot = false;
+            
+            if (activity.Type == "conversationUpdate")
+            {
+               
+                //check if bot is added
+                for (int i = 0; i < activity.MembersAdded.Count; i++)
+                {
+                    if (activity.MembersAdded[i].Id == activity.Recipient.Id)
+                    {
+                        addedBot = true;
+                        break;
+                    }
+                }               
+
+            }
+
             using (var connector = new ConnectorClient(new Uri(activity.ServiceUrl)))
             {
+
+                //register in TriviaApi
+                if (addedBot)
+                {
+                    var members = await connector.Conversations.GetConversationMembersAsync(activity.Conversation.Id);
+                    var teamid = activity.GetChannelData<TeamsChannelData>().Team.Id;
+                    var memberList = new List<TeamRosterMemberModel>();
+
+                    foreach (var member in members)
+                    {
+                        memberList.Add(new TeamRosterMemberModel(member.Id, member.Name));
+                    }
+
+                    TeamRosterModel teamRoster = new TeamRosterModel(teamid, memberList);
+
+                    var response = await triviaApi.TriviaRegisterTeamAsync(teamRoster);
+
+                    return new HttpResponseMessage(HttpStatusCode.Accepted);
+                }
+
+
+
                 if (activity.IsComposeExtensionQuery())
                 {
                     var response = MessageExtension.HandleMessageExtensionQuery(connector, activity);
@@ -32,5 +87,8 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web.Controllers
                 }
             }
         }
+
+
+        
     }
 }
--- Microsoft.Teams.Samples.HelloWorld.Web/EchoBot.cs
+++ Microsoft.Teams.Samples.HelloWorld.Web/EchoBot.cs
@@ -3,14 +3,32 @@
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Connector.Teams;
 
+using IO.Swagger.Api;
+using IO.Swagger.Client;
+using IO.Swagger.Model;
+
 namespace Microsoft.Teams.Samples.HelloWorld.Web
 {
     public class EchoBot
     {
         public static async Task EchoMessage(ConnectorClient connector, Activity activity)
         {
-            var reply = activity.CreateReply("You said: " + activity.GetTextWithoutMentions());
-            await connector.Conversations.ReplyToActivityWithRetriesAsync(reply);
+
+            var action = activity.GetTextWithoutMentions();
+            if (action.ToLower() == "trivia")
+            {
+                var triviaAPI = Microsoft.Teams.Samples.HelloWorld.Web.Controllers.MessagesController.triviaApi;
+
+                if (triviaAPI != null)
+                {
+                    
+
+                    //var question = triviaAPI.TriviaGetQuestionAsync();
+                }
+            }
+
+                var reply = activity.CreateReply("You said: " + activity.GetTextWithoutMentions());
+                await connector.Conversations.ReplyToActivityWithRetriesAsync(reply);
         }
     }
 }
