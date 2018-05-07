72412eaca81ea4c36b5444fd89296af7a4ced6cc
Stas Pavlov <stanisp@microsoft.com>
2018-05-07 14:12:07 +0300

    add AAD Auth

--- Microsoft.Teams.Samples.HelloWorld.Web/Dialogs/QuestionDialogcs.cs
+++ Microsoft.Teams.Samples.HelloWorld.Web/Dialogs/QuestionDialogcs.cs
@@ -8,6 +8,14 @@ using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
 using IO.Swagger.Model;
 
+using BotAuth.AADv2;
+using BotAuth.Dialogs;
+using BotAuth.Models;
+using BotAuth;
+
+using System.Configuration;
+using System.Threading;
+
 namespace Microsoft.Teams.Samples.HelloWorld.Web.Dialogs
 {
     [Serializable]
@@ -33,6 +41,33 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web.Dialogs
 
         public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
         {
+            var message = await result;
+
+
+
+            // Initialize AuthenticationOptions and forward to AuthDialog for token
+
+            AuthenticationOptions options = new AuthenticationOptions()
+
+            {
+
+                Authority = ConfigurationManager.AppSettings["aad:Authority"],
+                ClientId = ConfigurationManager.AppSettings["aad:ClientId"],
+                ClientSecret = ConfigurationManager.AppSettings["aad:ClientSecret"],
+                Scopes = new string[] { "User.Read" },
+                RedirectUrl = ConfigurationManager.AppSettings["aad:Callback"],
+                MagicNumberView = "/magic.html#{0}"
+            };
+
+            await context.Forward(new AuthDialog(new MSALAuthProvider(), options), async (IDialogContext authContext, IAwaitable<AuthResult> authResult) =>
+            {
+                var resultAuth = await authResult;
+
+                //// Use token to call into service
+                //var json = await new HttpClient().GetWithAuthAsync(result.AccessToken, "https://graph.microsoft.com/v1.0/me");
+                //await authContext.PostAsync($"I'm a simple bot that doesn't do much, but I know your name is {json.Value<string>("displayName")} and your UPN is {json.Value<string>("userPrincipalName")}");
+            }, message, CancellationToken.None);
+
             //Show options whatever users chat
             await QuastionDialog(context);
         }
